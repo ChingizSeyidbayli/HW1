ea4f57d5bb7f982816aff8995cd0007a3f81dd1e
Chingiz Seyidbayli <seyidbayli.chingiz@gmail.com>
2022-12-05 09:21:06 +0300

    Create e.py

---
+++ e.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+# Falling = 1
+# Standing = 2
+# Moving = 3
+
+# Value function = 3 x T 
+T=10
+V_vec  = np.zeros((3,T+1))
+
+P = np.zeros((3,3,3))
+
+
+P[0][0][0] = 0.6
+P[0][1][0] = 0.4
+P[1][2][0] = 1
+P[1][2][1] = 0.6
+P[1][2][0] = 0.4
+P[2][2][1] = 0.8
+P[2][0][1] = 0.2
+P[2][1][0] = 1
+
+r = np.zeros((3,3,3))
+
+r[0][0][0] = -1
+r[0][1][0] = 1
+r[1][2][0] = 1
+r[1][2][1] = 1
+r[1][2][0] = -1
+r[2][2][1] = 2
+r[2][0][1] = -1
+r[2][1][0] = 1
+
+
+# 0 represents slow, 1 represents fast
+PI = [[0],[1],[1]]*T
+
+
+
+
+for t in reversed(range(T)):
+    for i in range(3):
+        V = 0
+        for j in range(3):
+            a = PI[i][t]
+            n = (r[i,j,a])+(V_vec[j,t+1])
+            m = P[i,j,a]
+            V = V + m*n
+        V_vec[i][t] = V
+
+print(V_vec)
+        
